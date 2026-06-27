b620cb51b1a8da6f7b90ae6790e4ed051c6ac084
nikhil1-ux <nikhilyadav9044@gmail.com>
2026-06-27 05:42:20 +0530

    fix: Correct import casing for Cta component stylesheet

--- frontend/src/components/Cta.jsx
+++ frontend/src/components/Cta.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import '../style/CTA.css'
+import '../style/Cta.css'
 
 const CTA = () => {
   const navigate = useNavigate()
